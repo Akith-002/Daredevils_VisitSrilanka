aaef55b6f503fefcbdf028318e6ebdfb89761b9c
Akith-002 <chandinubaa.22@uom.lk>
2024-09-04 17:33:31 +0530

    initiate

---
+++ src/Components/NavbarType2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavbarType2() {
+  return (
+    <div className="fixed left-0 top-0 w-full h-5">
+      <p>VisitSrilanka</p>
+    </div>
+  );
+}
+
+export default NavbarType2;
---
+++ src/Views/VisaApprovalMain.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarType2 from "../Components/NavbarType2";
+
+function VisaApprovalMain() {
+  return (
+    <div>
+      <NavbarType2 />
+      Visa Approval Page
+    </div>
+  );
+}
+
+export default VisaApprovalMain;
