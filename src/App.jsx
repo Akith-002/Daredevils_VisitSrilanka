db32699f97349eae05ad35f43b382019af2fabfc
ShehanRajapaksha <shehan.rajapaksha2002@gmail.com>
2024-09-05 13:09:51 +0530

    Map component first phase added + .env created

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import VisaApprovalMain from "./Views/VisaApprovalMain";
 import { Router, BrowserRouter, Routes, Route } from "react-router-dom";
+import TourMap from "./Components/TourMap";
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/visaapprovalmain" Component={VisaApprovalMain} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <TourMap />
+    </div>
   );
 }
 
