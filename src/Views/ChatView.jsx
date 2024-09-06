6cf2c04f5149fa89ad0ccff8371d95e79e9008bd
thanura mendis <thanuramendis02@gmail.com>
2024-09-06 23:30:56 +0530

    ChatView NavBar added

--- src/Views/ChatView.jsx
+++ src/Views/ChatView.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Chat from "../Components/Chat";
 import TourMap from '../Components/TourMap';
+import NavbarType2 from '../Components/NavbarType2'
+import FooterType2 from '../Components/FooterType2'
 
 export default function ChatView() {
   return (
-    <div className="flex h-screen space-x-4 p-6 mx-4 ">
-      <div className="w-1/3">
-        <Chat />
+    <div>
+      <NavbarType2 />
+      <div className="mt-12 flex h-[70vh] space-x-4 p-6 mx-4 ">
+        <div className="w-1/3">
+          <Chat />
+        </div>
+        <div className="w-2/3">
+          <TourMap />
+        </div>
       </div>
-      <div className="w-2/3">
-        <TourMap />
-      </div>
+   
     </div>
   );
 }
