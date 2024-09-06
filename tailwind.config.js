fccfbdc3677246488974dd2ef771914a6df68ee9
Sanidi-Sisara <sanidi1113@gmail.com>
2024-09-06 20:47:53 +0530

    home page animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,7 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        "slide-in": {
-          "0%": { transform: "translateX(-100%)", opacity: "0" },
-          "100%": { transform: "translateX(0)", opacity: "1" },
-        },
-      },
-      animation: {
-        "slide-in": "slide-in 1s ease-out forwards",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 });
